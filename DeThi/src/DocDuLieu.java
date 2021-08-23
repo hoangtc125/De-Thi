59f5868a478213e5637489e1faee0a499a6c99ab
hoangtc125 <anhhoangday.69@gmail.com>
2021-08-23 09:21:41 +0700

    more Question updated and auto spliting answers per sentencen

--- DeThi/src/DocDuLieu.java
+++ DeThi/src/DocDuLieu.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class DocDuLieu {
+	static public int numbersOfQuestion = 0;
 	static public ArrayList<CauHoi> doc() throws IOException {
 		ArrayList<CauHoi> cauHoi = new ArrayList<CauHoi>();
-		FileReader fr = new FileReader("src\\ngan hang cau hoi.txt");
+		FileReader fr = new FileReader("src\\ngan hang cau hoi 2.txt");
         BufferedReader br = new BufferedReader(fr);
   
         String string;
-        int count = 0;
-        int index = 0;
         while ((string = br.readLine()) != null) {
         	if(string.length() <= 0) {
         		continue;
@@ -22,24 +21,49 @@ public class DocDuLieu {
                  	CauHoi c = new CauHoi();
                  	c.setDebai(string);
                  	cauHoi.add(c);
-                 	count++;
+                 	numbersOfQuestion++;
 				} catch (Exception e) {
 					// TODO: handle exception
 					continue;
 				}
-            }
+            } else {
+            	StringBuilder stringBuilder = new StringBuilder(string);
+                stringBuilder.insert(0, " ");
+                int count = 0;
+                ArrayList<Integer> index = new ArrayList<Integer>();
+                while(stringBuilder.toString().indexOf(".", count) != -1 && count < stringBuilder.toString().length() - 1) {
+                	count = stringBuilder.toString().indexOf(".", count);
+                	if(count != stringBuilder.toString().length() - 1) index.add(count);
+                	count++;
+                }
+                if(index.size() > 1) {
+                	for(int i = 0; i < index.size(); i++) {
+                    	DapAn d = new DapAn();
+            			d.setString(stringBuilder.toString().substring(index.get(i) - 2, (i + 1) < index.size() ? index.get(i + 1) - 2 : stringBuilder.toString().length()));
+            			if(d.getString().contains("*")) {
+            				d.setDapan(1);
+            				d.setString(d.getString().substring(1));
+            			} else {
+        					d.setDapan(0);
+        					d.setString(d.getString().substring(0));
+        				}
+                		cauHoi.get(numbersOfQuestion - 1).addDapAn(d);
+                    }
+                } else {
+                	if(string.charAt(0) == '*' || string.charAt(1) == '.' || string.charAt(1) == ')') {
+                		DapAn d = new DapAn();
+                		if(string.charAt(0) == '*') {
+                			d.setString(string.substring(1));
+                			d.setDapan(1);
+                		} else {
+        					d.setString(string);
+        					d.setDapan(0);
+        				}
+                		cauHoi.get(numbersOfQuestion - 1).addDapAn(d);
+                	}
+    			}
+			}
             
-            if(string.charAt(0) == '*' || string.charAt(1) == '.' || string.charAt(1) == ')') {
-        		DapAn d = new DapAn();
-        		if(string.charAt(0) == '*') {
-        			d.setString(string.substring(1));
-        			d.setDapan(1);
-        		} else {
-					d.setString(string);
-					d.setDapan(0);
-				}
-        		cauHoi.get(count - 1).addDapAn(d);
-        	}
         }
         br.close();
         fr.close();
--- DeThi/src/GUI.java
+++ DeThi/src/GUI.java
@@ -1,10 +1,14 @@
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.text.BreakIterator;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -25,12 +29,15 @@ import javax.swing.JLabel;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 import javax.swing.Action;
+import javax.swing.SwingConstants;
 
 public class GUI implements ActionListener {
 
 	private JFrame frame;
-	private JTextArea jtaDeBai = new JTextArea(5, 0);
-	private JTextField JfdiemSo = new JTextField();
+	private JTextArea jaDeBai = new JTextArea(5, 0);
+	private JTextField jdDiemso = new JTextField(),
+					   jdInput = new JTextField(),
+					   jdOutput = new JTextField();
 	private JRadioButton [] jrDapAn = new JRadioButton[10];
 	private ArrayList<CauHoi> cauHoi = new ArrayList<CauHoi>();
 	private JTextArea [] jtaDapAn = new JTextArea[10];
@@ -46,12 +53,14 @@ public class GUI implements ActionListener {
 					rightPanel = new JPanel(new BorderLayout()),
 					centerRightPanel = new JPanel(new BorderLayout()),
 					endRightPanel = new JPanel();
-	private JButton [] btnNumber = new JButton[250];
+//	private JButton [] btnNumber = new JButton[250];
+	private ArrayList<JButton> btnNumber = new ArrayList<JButton>();
 	private JPanel [] menuPanel = new JPanel[5];
 	private int	numberOfAnswers,
 				current = 0,
 				score = 0,
 				currentPage = 0;
+	private Scanner scanner = new Scanner(jdInput.getText());
 	private Font font = new Font("Aria", Font.BOLD, 20);
 	private ArrayList<Integer> doneAnswer = new ArrayList<Integer>();
 
@@ -92,18 +101,18 @@ public class GUI implements ActionListener {
 		
 		cauHoi = DocDuLieu.doc();
 		
-		JfdiemSo.setText("Score: 0");
-		JfdiemSo.setEditable(false);
-		JfdiemSo.setFont(font);
-		JfdiemSo.setBackground(Color.white);
+		jdDiemso.setText("Score: 0");
+		jdDiemso.setEditable(false);
+		jdDiemso.setFont(font);
+		jdDiemso.setBackground(Color.white);
 		for(int i = 0; i < menuPanel.length; i++) {
 			menuPanel[i] = new JPanel(new GridLayout(10, 5, 0, 0));
 		}
-		for(int i = 0; i < btnNumber.length; i++) {
-			btnNumber[i] = new JButton((i + 1) + "");
-			btnNumber[i].addActionListener(this);
-			btnNumber[i].setBackground(Color.white);
-			menuPanel[i / 50].add(btnNumber[i]);
+		for(int i = 0; i < DocDuLieu.numbersOfQuestion; i++) {
+			btnNumber.add(new JButton((i + 1) + ""));
+			btnNumber.get(i).addActionListener(this);
+//			btnNumber.get(i).addMouseListener(new customMouseListener(i, -1));
+			btnNumber.get(i).setBackground(Color.white);
 		}
 		btnMenuPrev.addActionListener(this);
 		btnMenuPrev.setBackground(Color.white);
@@ -111,15 +120,24 @@ public class GUI implements ActionListener {
 		btnMenuNext.setBackground(Color.white);
 //		rightPanel.add(menuPanel[currentPage], BorderLayout.CENTER);
 		rightPanel.add(centerRightPanel, BorderLayout.CENTER);
-		rightPanel.add(JfdiemSo, BorderLayout.PAGE_START);
+		rightPanel.add(jdDiemso, BorderLayout.PAGE_START);
 		endRightPanel = new JPanel(new GridLayout(3, 1, 0, 0));
 		endRightPanel.setBackground(Color.white);
 //		endRightPanel.add(new JLabel(" "));
-		endRightPanel.add(new JLabel(" Page sẽ tự sang trang kế khi làm qua câu cuối cùng"));
+		JLabel label_2 = new JLabel("Page sẽ tự sang trang kế khi làm qua câu cuối cùng");
+		label_2.setHorizontalAlignment(SwingConstants.CENTER);
+		endRightPanel.add(label_2);
+		JLabel label_1 = new JLabel("Sau khi submit hãy bấm Next/Previous Question ");
+		label_1.setHorizontalAlignment(SwingConstants.CENTER);
+		endRightPanel.add(label_1);
+		JLabel label = new JLabel("Numbers of question: " + DocDuLieu.numbersOfQuestion);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		endRightPanel.add(label);
 //		endRightPanel.add(btnMenuPrev);
-		endRightPanel.add(new JLabel(" "));
+//		endRightPanel.add(jdInput);
+//		jdInput.addMouseListener(new customMouseListener());
 //		endRightPanel.add(btnMenuNext);
-		endRightPanel.add(new JLabel(" "));
+//		endRightPanel.add(jdOutput);
 		rightPanel.add(endRightPanel, BorderLayout.PAGE_END);
 		rightPanel.setPreferredSize(new Dimension(300, 500));
 		frame.getContentPane().add(rightPanel, BorderLayout.EAST);		
@@ -135,12 +153,12 @@ public class GUI implements ActionListener {
 		btnPrev.setBackground(Color.white);
 		btnPrev.setEnabled(false);
 		btnPrev.setFont(font);
-		JPanel endPanel = new JPanel(new GridLayout(1, 5, 0, 0));
+//		JPanel endPanel = new JPanel(new GridLayout(1, 5, 0, 0));
+		JPanel endPanel = new JPanel(new BorderLayout());
 		endPanel.setBackground(Color.white);
-		endPanel.add(btnPrev);
-		endPanel.add(btnSubmit);
-		endPanel.add(btnNext);
-//		endPanel.add(new JLabel("Page sẽ tự sang trang kế khi làm qua câu cuối cùng"));
+		endPanel.add(btnPrev, BorderLayout.WEST);
+		endPanel.add(btnSubmit, BorderLayout.CENTER);
+		endPanel.add(btnNext, BorderLayout.EAST);
 		frame.getContentPane().add(endPanel, BorderLayout.PAGE_END);
 		
 		display();
@@ -170,10 +188,10 @@ public class GUI implements ActionListener {
 	
 	private void display() {
 		
-		jtaDeBai.setText(cutString(cauHoi.get(current).getDebai()) + (cauHoi.get(current).getCorrectAnswer() > 1 ? "\n\n(Có thể chọn nhiều hơn 1 đáp án)" : "\n\n(Chỉ chọn 1 đáp án)"));
-		jtaDeBai.setEditable(false);
-		jtaDeBai.setFont(font);
-		btnNumber[current].setBackground(Color.yellow);
+		jaDeBai.setText(cutString(cauHoi.get(current).getDebai()) + (cauHoi.get(current).getCorrectAnswer() > 1 ? "\n\n(Có thể chọn nhiều hơn 1 đáp án)" : "\n\n(Chỉ chọn 1 đáp án)"));
+		jaDeBai.setEditable(false);
+		jaDeBai.setFont(font);
+		btnNumber.get(current).setBackground(Color.yellow);
 		
 		numberOfAnswers = cauHoi.get(current).getDapAn().size();
 		btnGroup = new ButtonGroup();
@@ -183,11 +201,12 @@ public class GUI implements ActionListener {
 		for(int i = 0; i < numberOfAnswers; i++) { 
 			answerPanel.add(jrDapAn[i] = createRadioButton(cutString(cauHoi.get(current).getDapAn().get(i).getString().trim()), false));
 			jrDapAn[i].setFont(font);
+//			jrDapAn[i].addMouseListener(new customMouseListener(-1, i));
 			if(cauHoi.get(current).getCorrectAnswer() <= 1) {
 				btnGroup.add(jrDapAn[i]);
 			}
 		}
-		frame.getContentPane().add(jtaDeBai, BorderLayout.NORTH);
+		frame.getContentPane().add(jaDeBai, BorderLayout.NORTH);
 		frame.getContentPane().add(answerPanel, BorderLayout.CENTER);
 	}
 	
@@ -195,8 +214,8 @@ public class GUI implements ActionListener {
 		centerRightPanel.removeAll();
 		centerRightPanel = new JPanel(new GridLayout(10, 5, 0, 0));
 		centerRightPanel.setBackground(Color.white);
-		for(int i = 50 * currentPage; i < 50 * currentPage + 50; i++) {
-			centerRightPanel.add(btnNumber[i]);
+		for(int i = 50 * currentPage; i < (50 * currentPage + 50 < DocDuLieu.numbersOfQuestion ? 50 * currentPage + 50 : DocDuLieu.numbersOfQuestion) ; i++) {
+			centerRightPanel.add(btnNumber.get(i));
 		}
 		rightPanel.add(centerRightPanel, BorderLayout.CENTER);
 	}
@@ -240,10 +259,10 @@ public class GUI implements ActionListener {
 				}
 				if(mark) {
 					score++;
-					JfdiemSo.setText("Score: " + score);
-					btnNumber[current].setBackground(Color.green);
+					jdDiemso.setText("Score: " + score);
+					btnNumber.get(current).setBackground(Color.green);
 				} else {
-					btnNumber[current].setBackground(Color.red);
+					btnNumber.get(current).setBackground(Color.red);
 					for(int i = 0; i < chooser.size(); i++) {
 						if(!cauHoi.get(current).getDapAnDung().contains(chooser.get(i))) {
 							jrDapAn[chooser.get(i)].setBackground(Color.red);
@@ -251,7 +270,7 @@ public class GUI implements ActionListener {
 					}
 				}
 			} else {
-				btnNumber[current].setBackground(Color.red);
+				btnNumber.get(current).setBackground(Color.red);
 				for(int i = 0; i < chooser.size(); i++) {
 					if(!cauHoi.get(current).getDapAnDung().contains(chooser.get(i))) {
 						jrDapAn[chooser.get(i)].setBackground(Color.red);
@@ -264,10 +283,51 @@ public class GUI implements ActionListener {
 		}
 	}
 	
+	class customMouseListener implements MouseListener {
+		private int i = -1;
+		private int j = -1;
+		public customMouseListener(int i, int j) {
+			// TODO Auto-generated constructor stub
+			this.i = i;
+			this.j = j;
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			if(i > -1) btnNumber.get(i).setBackground(Color.yellow);
+			if(j > -1 && !jrDapAn[j].isSelected() && btnSubmit.isEnabled()) jrDapAn[j].setBackground(Color.yellow);
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			if(i > -1) btnNumber.get(i).setBackground(Color.white);
+			if(j > -1 && !jrDapAn[j].isSelected() && btnSubmit.isEnabled()) jrDapAn[j].setBackground(Color.white);
+		}
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource() == btnNext && current < 250) {
+		if(e.getSource() == btnNext && current < DocDuLieu.numbersOfQuestion) {
 			current++;
 			setUp();
 			if(current / 50 > currentPage) {
@@ -283,7 +343,7 @@ public class GUI implements ActionListener {
 				displayPage();
 			}
 		}
-		if(e.getSource() == btnMenuNext && currentPage < 4) {
+		if(e.getSource() == btnMenuNext && currentPage < DocDuLieu.numbersOfQuestion / 50) {
 			currentPage++;
 			displayPage();
 		}
@@ -291,10 +351,10 @@ public class GUI implements ActionListener {
 			currentPage--;
 			displayPage();
 		}
-		for(int i = 0; i < btnNumber.length; i++) {
-			if(e.getSource() == btnNumber[i]) {
+		for(int i = 0; i < DocDuLieu.numbersOfQuestion; i++) {
+			if(e.getSource() == btnNumber.get(i)) {
 				if(current == i) break;
-				btnNumber[current].setBackground(Color.white);
+				btnNumber.get(current).setBackground(Color.white);
 				current = i;
 				setUp();
 			}
